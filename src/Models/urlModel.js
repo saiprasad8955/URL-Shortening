0f0cc8411f98f31a14fecd89504192f0beac78fc
saiprasad <rsaiprasad4@gmail.com>
2022-05-17 23:57:51 +0530

    done

--- src/Models/urlModel.js
+++ src/Models/urlModel.js
@@ -4,10 +4,10 @@ const urlSchema = new mongoose.Schema({
 
     urlCode:{ type:String, required:true , lowercase:true, trim:true },
 
-    longUrl:{type:String, required:true },
+    longUrl: { type:String, required:true },
 
-    shortUrl:{type:String, unique:true, required:true}
+    shortUrl:{ type:String, unique:true, required:true}
 
-},{timeseries:true});
+},{timestamps:true});
 
 module.exports = mongoose.model("URl", urlSchema );
