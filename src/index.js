f10ecbddefe98159fa718463e126c915d1ed7b25
saiprasad <rsaiprasad4@gmail.com>
2022-07-10 21:01:17 +0530

    ENV FILE ADDED

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const express = require("express") ;
+require('dotenv').config()
+const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
@@ -6,15 +7,14 @@ const route = require("./route/route");
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : true })) ;
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://Sai0047:rXxgqYKPqwnhcXX7@cluster0.qptsw.mongodb.net/group59Database"
-,{useNewUrlParser:true})
-.then ( (   ) => { console.log( "MongoDB is Successfully Connected😍😍") })
-.catch( (err) => { console.log( err.message                             ) });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+    .then(() => { console.log("MongoDB is Successfully Connected😍😍") })
+    .catch((err) => { console.log(err.message) });
 
 app.use('/', route);
 
-app.listen( process.env.PORT || 3000 , function () {
-    console.log("Express app is running on port " + (process.env.PORT || 3000 )+"😍😍");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Express app is running on port " + (process.env.PORT || 3000) + "😍😍");
 });
